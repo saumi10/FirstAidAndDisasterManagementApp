7f4fe49a5de73c9f640c856622d79e82d96c2cc9
saumi10 <saumi2310@gmail.com>
2023-06-07 11:09:20 +0530

    splash screen added

---
+++ app/src/main/java/com/example/android/firstaidanddisastermanagementapp/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.example.android.firstaidanddisastermanagementapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreenActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
