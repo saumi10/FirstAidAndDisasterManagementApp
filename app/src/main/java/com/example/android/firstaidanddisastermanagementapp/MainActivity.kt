c2d81bc2b6c51ab1f560b8ea0468031e387917a4
saumi10 <saumi2310@gmail.com>
2023-06-10 02:57:05 +0530

    HOME PAGE UI COMPLETED

--- app/src/main/java/com/example/android/firstaidanddisastermanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/android/firstaidanddisastermanagementapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android.firstaidanddisastermanagementapp
 
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -25,7 +26,9 @@ class MainActivity : AppCompatActivity() {
 
         // to make the Navigation drawer icon always appear on the action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        getSupportActionBar()?.setTitle("");
     }
+
 
     // override the onOptionsItemSelected()
     // function to implement
